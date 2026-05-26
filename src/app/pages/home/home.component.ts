52a2a6bda44afa6d0e5b6c81c6ca0c37c8add083
Daniel <danieldeepakn@gmail.com>
2026-05-26 13:33:04 +0530

    Added async dashboard loading experience

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { StatisticsComponent } from '../../components/statistics/statistics.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [StatisticsComponent],
+  imports: [CommonModule, StatisticsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+
+  loading = true;
+
+  ngOnInit(): void {
+
+    // Simulated async API delay
+    setTimeout(() => {
+      this.loading = false;
+    }, 2000);
+
+  }
+}
